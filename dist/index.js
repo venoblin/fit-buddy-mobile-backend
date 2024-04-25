d74484c6cb362d495684933ccdb057d819ff9a30
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-04-24 23:00:12 -0400

    added starting point for server

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,7 @@
 "use strict";
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
